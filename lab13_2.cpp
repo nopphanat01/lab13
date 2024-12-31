c4aa20440555b051298fb9d57f1b2960f409769a
nopphanat01 <sspas401@gmail.com>
2024-12-31 18:04:47 +0700

    1

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -5,49 +5,53 @@ using namespace std;
 const int N = 30;
 const int M = 70;
 
-void updateImage(bool image[][M], int s, int x, int y);
-void showImage(const bool image[][M]);
+void updateImage(bool [][M],int,int,int);
+
+void showImage(const bool [][M]);
 
 int main()
 {
     bool image[N][M] = {};
-    int s, x, y;
+    int s,x,y;
     do{
         showImage(image);
         cout << "Input your brush size and location: ";
         cin >> s >> x >> y;
-        updateImage(image, s, x, y);
+        updateImage(image,s,x,y);
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
 
-void updateImage(bool image[][M], int s, int x, int y)
-{
-    for(int i = 0; i < N; ++i)
-    {
-        for(int j = 0; j < M; ++j)
-        {
-            if(sqrt(pow(i - x, 2) + pow(j - y, 2)) <= s) 
-            {
-                if(i >= 0 && i < N && j >= 0 && j < M) {
-                    image[i][j] = true;
-                }
-            }
-        }
-    }
-}
+void updateImage(bool T[N][M],int s,int x,int y){
+    for(int l=0;l<N;l++){
+        for(int h=0;h<M;h++){
+            int dx = l-x;
+            int dy = h-y;
+            double distant = sqrt(pow(dx,2)+pow(dy,2));
+            if(distant <= s-1) T[l][h]=true;
 
-void showImage(const bool image[][M])
-{
-    for(int i = 0; i < N; ++i)
-    {
-        for(int j = 0; j < M; ++j)
-        {
-            if(image[i][j])
-                cout << "*";
-            else
-                cout << " ";
         }
-        cout << endl;
     }
+    
+    }
+    
+    
+
+void showImage(const bool T[N][M]){
+    for(int c=0;c<M+2;c++){
+        cout << "-";
+    }
+    cout << "\n";
+    for(int a=0;a<N;a++){
+        cout<<"|";
+        for(int b=0;b<M;b++){
+            if(T[a][b]==1) cout <<"*";
+            else cout << " ";
+        }
+        cout<<"|\n";
+    }
+    for(int c=0;c<M+2;c++){
+        cout << "-";
+    }
+    cout << "\n";
 }
