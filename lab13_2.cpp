d616ee23521f335a017d1ca67eb1348f55e693dd
nopphanat01 <sspas401@gmail.com>
2024-12-31 13:27:51 +0700

    1

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -5,19 +5,49 @@ using namespace std;
 const int N = 30;
 const int M = 70;
 
-void updateImage(bool [][M],int,int,int);
-
-void showImage(const bool [][M]);
+void updateImage(bool image[][M], int s, int x, int y);
+void showImage(const bool image[][M]);
 
 int main()
 {
     bool image[N][M] = {};
-    int s,x,y;
+    int s, x, y;
     do{
         showImage(image);
         cout << "Input your brush size and location: ";
         cin >> s >> x >> y;
-        updateImage(image,s,x,y);
+        updateImage(image, s, x, y);
     }while(s != 0 || x != 0 || y != 0);
     return 0;
+}
+
+void updateImage(bool image[][M], int s, int x, int y)
+{
+    for(int i = 0; i < N; ++i)
+    {
+        for(int j = 0; j < M; ++j)
+        {
+            if(sqrt(pow(i - x, 2) + pow(j - y, 2)) <= s) 
+            {
+                if(i >= 0 && i < N && j >= 0 && j < M) {
+                    image[i][j] = true;
+                }
+            }
+        }
+    }
+}
+
+void showImage(const bool image[][M])
+{
+    for(int i = 0; i < N; ++i)
+    {
+        for(int j = 0; j < M; ++j)
+        {
+            if(image[i][j])
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
 }
